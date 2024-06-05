e6b7306d563c77f88114e5b9d242cab95d20b8ad
Ragex <ragexx1965@gmail.com>
2024-06-05 18:18:55 -0500

    FIRST UPLOAD

--- bot/commands/get_fichaAzul.js
+++ bot/commands/get_fichaAzul.js
@@ -221,11 +221,22 @@ module.exports = (bot) => {
     usuariosEnConsulta[userId] = true;
 
     try {
-
       const responseFichaAzul = await fichaAzul(dni);
+
+      if (
+        responseFichaAzul.error ===
+        `El DNI ${dni} no cuenta con datos disponibles para la construcción de la ficha`
+      ) {
+        await bot.deleteMessage(chatId, consultandoMessage.message_id);
+
+        let yx = `*[✖️] El DNI ${dni}* no cuenta con datos suficientes para la construcción *de la ficha*.`;
+
+        return bot.sendMessage(chatId, yx, messageOptions);
+      }
+
       const listaAni = responseFichaAzul.listaAni;
       console.log("LISTA ANI FX AZUL: " + listaAni);
-      
+
       const {
         apeMaterno, // Apellido materno
         apePaterno, // Apellido paterno
