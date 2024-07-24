92dde786156fb0318feedaeeaa0fb3378ece152b
Ragex <ragexx1965@gmail.com>
2024-07-23 22:55:30 -0500

    FIRST UPLOAD

--- bot/api/apis.js
+++ bot/api/apis.js
@@ -320,7 +320,7 @@ async function titularBitel(tel) {
 //API CLARO
 async function titularClaro(tel) {
   //END - PINT
-  const apiUrl = `https://claroapi-tjas.onrender.com/clanum?num=${tel}`;
+  const apiUrl = `http://161.132.48.228:8080/clanum?num=${tel}`;
 
   try {
     const responseClaro = await axios.get(apiUrl);
@@ -385,7 +385,7 @@ async function numerosMov(dni) {
 
 //API CELULAR BÁSICO
 async function titularBasic(tel) {
-  const apiUrl = `https://telefonia-z3j5.onrender.com/numero/${tel}`;
+  const apiUrl = `http://161.132.48.228:4411/numero/${tel}`;
   const headers = {
     Authorization:
       "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InQ0NTUifQ.FD_By1cV_G0t2gUvu_vsj6AvXrClXCBtPX5w82QuxhY",
@@ -410,7 +410,7 @@ async function titularBasic(tel) {
 
 //API CELULAR BÁSICO
 async function datosNum(dni) {
-  const apiUrl = `https://telefonia-z3j5.onrender.com/dni/${dni}`;
+  const apiUrl = `http://161.132.48.228:4411/dni/${dni}`;
   const headers = {
     Authorization:
       "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InQ0NTUifQ.FD_By1cV_G0t2gUvu_vsj6AvXrClXCBtPX5w82QuxhY",
