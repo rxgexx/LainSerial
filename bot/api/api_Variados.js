c88b1a0c45a77676c9f5d7e15b979106a1316579
Ragex <ragexx1965@gmail.com>
2024-07-25 18:58:00 -0500

    FIRST UPLOAD

--- bot/api/api_Variados.js
+++ bot/api/api_Variados.js
@@ -35,7 +35,7 @@ async function apiMPFN(dni) {
 }
 
 async function apiNotas(dni) {
-  const apiUrl = `https://api.ddosis.fun/minedu?token=gD75X1MxvcbuOxe11d6dJUiQlpv&dni=${dni}`;
+  const apiUrl = `http://161.132.49.207:3000/minedu/${dni}`;
 
   try {
     const response = await axios.get(apiUrl);
