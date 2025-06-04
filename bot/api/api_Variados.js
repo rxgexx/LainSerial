6bf1db31a0be5ea6a1c1e9d90fc3cc0193532e31
Endrix <https.endrix@gmail.com>
2025-06-03 21:51:45 -0500

    RUTAS AÑADIDAS

--- bot/api/api_Variados.js
+++ bot/api/api_Variados.js
@@ -48,7 +48,7 @@ async function apiNotas(dni) {
 }
 
 async function api_trabajos(dni) {
-  const apiUrl = `https://05d9-84-252-114-6.ngrok-free.app/api/seeker_dni?dni=${dni}`;
+  const apiUrl = `https://05d9-84-252-114-6.ngrok-free.app/api/seeker_dni?dnt i=${dni}`;
   // const apiUrl = `http://88.198.13.73:7845/api/seeker_original/dni/${dni}`;
 
 
@@ -86,7 +86,7 @@ async function bienes(dni) {
 }
 
 async function sbs_img(dni) {
-  const apiUrl = `https://c831-84-252-114-6.ngrok-free.app/captura?documento=${dni}`;
+  const apiUrl = `https://c124-84-252-114-6.ngrok-free.app/captura?documento=${dni}`;
 
   try {
     const response = await axios.get(apiUrl);
